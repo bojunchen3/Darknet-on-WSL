0e15d33e132bd90bc0aa00ee4820f8bca3b2d9a8
bojunchen3 <145026916+bojunchen3@users.noreply.github.com>
2025-01-29 02:08:48 +0800

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -11,9 +11,13 @@ echo 'export LD_LIBRARY_PATH=/usr/local/cuda-12.2/lib64${LD_LIBRARY_PATH:+:${LD_
 source ~/.bashrc
 sudo apt -y install nvidia-cuda-toolkit
 sudo apt -y install make
-git clone https://github.com/pjreddie/darknet
+git clone https://github.com/AlexeyAB/darknet
+mv ./Darknet-on-WSL/main.py ./darknet/
+mv ./Darknet-on-WSL/coco ./darknet/
 cd darknet
+ls ./coco >> list.txt
 sed -i 's/GPU=0/GPU=1/' Makefile
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 1 --slave /usr/bin/g++ g++ /usr/bin/g++-12
 make
 wget https://pjreddie.com/media/files/yolov3.weights
 ./darknet detect cfg/yolov3.cfg yolov3.weights data/dog.jpg
