1c982f302f1d9c3bfe7ca4c32e136a6e5b6470e5
bojunchen3 <145026916+bojunchen3@users.noreply.github.com>
2025-02-11 01:37:31 +0800

    Add files via upload

--- cudnn.sh
+++ cudnn.sh
@@ -7,6 +7,9 @@ sudo cp -r ./lib/* /usr/local/cudnn/lib64/
 sudo cp -r ./include/* /usr/local/cudnn/include/
 sudo chmod a+r /usr/local/cudnn/include/cudnn*
 sudo chmod a+r /usr/local/cudnn/lib64/libcudnn*
+echo 'export LD_LIBRARY_PATH=/usr/local/cudnn/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}' >> ~/.bashrc
+echo 'export PATH=/usr/local/cudnn/include${PATH:+:${PATH}}' >> ~/.bashrc
+source ~/.bashrc
 cd ../darknet
 sed -i 's/CUDNN=0/CUDNN=1/' Makefile
 make
