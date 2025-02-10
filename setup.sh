94fd35fe8e2b5146ad504ee4d0a4d3733f225f55
bojunchen3 <145026916+bojunchen3@users.noreply.github.com>
2025-02-10 19:04:41 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,6 @@
 cd
-wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin
-sudo mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/12.2.2/local_installers/cuda-repo-wsl-ubuntu-12-2-local_12.2.2-1_amd64.deb
-sudo dpkg -i cuda-repo-wsl-ubuntu-12-2-local_12.2.2-1_amd64.deb
-sudo cp /var/cuda-repo-wsl-ubuntu-12-2-local/cuda-*-keyring.gpg /usr/share/keyrings/
-sudo apt-get update
-sudo apt-get -y install cuda
-echo 'export PATH=/usr/local/cuda-12.2/include${PATH:+:${PATH}}' >> ~/.bashrc
-echo 'export LD_LIBRARY_PATH=/usr/local/cuda-12.2/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}' >> ~/.bashrc
-source ~/.bashrc
+sudo apt update
+sudo apt upgrade
 sudo apt -y install nvidia-cuda-toolkit
 sudo apt -y install make
 git clone https://github.com/AlexeyAB/darknet
