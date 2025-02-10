2797e5b1f3aed5f78b5d2023de26bebc124e6591
bojunchen3 <145026916+bojunchen3@users.noreply.github.com>
2025-02-10 21:15:20 +0800

    Update cudnn.sh

--- cudnn.sh
+++ cudnn.sh
@@ -1,5 +1,5 @@
 cd
-#sudo tar -xvf ./cudnn*xz
+sudo tar -xvf ./cudnn*xz
 cd ./cudnn*archive
 sudo mkdir -p /usr/local/cudnn/lib64
 sudo mkdir -p /usr/local/cudnn/include
@@ -9,7 +9,7 @@ sudo chmod a+r /usr/local/cudnn/include/cudnn*
 sudo chmod a+r /usr/local/cudnn/lib64/libcudnn*
 cd ../darknet
 sed -i 's/CUDNN=0/CUDNN=1/' Makefile
-#sed -i 's|local/cuda/lib64|lib/cuda/lib64|/g' Makefile
-#sed -i 's|local/cuda/include/|include/|g' Makefile
+sed -i 's|local/cuda/lib64|lib/cuda/lib64|/g' Makefile
+sed -i 's|local/cuda/include/|include/|g' Makefile
 make
 ./darknet detect cfg/yolov3.cfg yolov3.weights data/dog.jpg
